0293f5694bd3de7423cffc792bee09cbbbc21c0d
Mykhailo Halchenko <114739882+MykhailoHalchenko@users.noreply.github.com>
2025-07-13 16:46:35 +0300

    Log

--- SlotMachineApp/ContentView.swift
+++ SlotMachineApp/ContentView.swift
@@ -1,27 +1,31 @@
 import SwiftUI
 
 
-struct Hexagonal: Shape {
+struct Hexagon: Shape {
     func path(in rect: CGRect) -> Path {
-        return Path {path in
+        return Path { path in
             let p1 = CGPoint(x: 0, y: 20)
-                        let p2 = CGPoint(x: 0, y: rect.height - 20)
-                        let p3 = CGPoint(x: rect.width/2, y: rect.height)
-                        let p4 = CGPoint(x: rect.width, y: rect.height - 20)
-                        let p5 = CGPoint(x: rect.width, y: 20)
-                        let p6 = CGPoint(x: rect.width/2, y: 0)
-                        
-                        path .move(to: p6)
-                        
-                        
-                        path.addArc(tangent1End: p1, tangent2End: p2, radius: 15)
-                        path.addArc(tangent1End: p2, tangent2End: p3, radius: 15)
-                        path.addArc(tangent1End: p3, tangent2End: p4, radius: 15)
-                        path.addArc(tangent1End: p4, tangent2End: p5, radius: 15)
-                        path.addArc(tangent1End: p5, tangent2End: p6, radius: 15)
-                        path.addArc(tangent1End: p6, tangent2End: p1, radius: 15)
+            let p2 = CGPoint(x: 0, y: rect.height - 20)
+            let p3 = CGPoint(x: rect.width/2, y: rect.height)
+            let p4 = CGPoint(x: rect.width, y: rect.height - 20)
+            let p5 = CGPoint(x: rect.width, y: 20)
+            let p6 = CGPoint(x: rect.width/2, y: 0)
+            
+            path .move(to: p6)
+            
+            
+            path.addArc(tangent1End: p1, tangent2End: p2, radius: 15)
+            path.addArc(tangent1End: p2, tangent2End: p3, radius: 15)
+            path.addArc(tangent1End: p3, tangent2End: p4, radius: 15)
+            path.addArc(tangent1End: p4, tangent2End: p5, radius: 15)
+            path.addArc(tangent1End: p5, tangent2End: p6, radius: 15)
+            path.addArc(tangent1End: p6, tangent2End: p1, radius: 15)
+            
+            
         }
     }
+    
+    
 }
 
 enum Choice: Int, Identifiable {
@@ -38,13 +42,16 @@ struct ContentView: View {
     @State public var counter = 0
     @State private var showingAlert: Choice?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("sunshine")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack(alignment: .center, spacing: 80) {
+                HStack {
+                    Image("fire")
+                }
+            }
         }
-        .padding()
     }
 }
 
